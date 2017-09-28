dac5943b3d9ff748d81d8998dfdd1c66a10dabaf
Besttdk <Besttdk@ASSELBY>
2017-09-29 03:45:54 +0600

    Добавьте файлы проекта.

---
+++ WindowsFormsHW3/Form1.cs
@@ -0,0 +1,39 @@
+﻿/*Написать программу, которая по введенной дате определяет день
+недели. Результат выводить в текстовое поле (желательно по-русски). */
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsHW3
+{
+    public partial class Form1 : Form
+    {
+        DateTime date = new DateTime();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Button1_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Right)
+            {
+                //  MessageBox.Show(String.Format("", dateTimePicker2))
+            }
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            date = dateTimePicker2.Value;
+            MessageBox.Show(date.ToString("dddd", CultureInfo.GetCultureInfo("ru-ru")));
+        }
+    }
+}
+
